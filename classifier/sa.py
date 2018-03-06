c2fd704d1fc48418a0c7ab3abc25d2b8a2a9fdc0
Irene <irene.nikkarinen@helsinki.fi>
2018-03-06 21:06:44 +0200

    Update parameters based on amazon data

--- classifier/sa.py
+++ classifier/sa.py
@@ -4,6 +4,7 @@ from os.path import isfile
 import sys
 import csv
 import re
+import time
 from random import shuffle
 import torch
 from torch import nn
@@ -16,23 +17,26 @@ from functools import reduce
 rgx = re.compile(r"\d")
 splitter = re.compile(r"[?!.]")
 
-
 class Entwork(nn.Module):
     def __init__(self):
         super(Entwork, self).__init__()
         self.cv1 = nn.Conv2d(1, 6, (3, 20))
         self.cv2 = nn.Conv2d(6, 16, (3, 20))
-        self.fc1 = nn.Linear(384, 200)
-        self.fc2 = nn.Linear(200, 50)
-        self.fc3 = nn.Linear(50, 5)
+        self.fc1 = nn.Linear(1024, 200)
+        self.fc2 = nn.Linear(200, 200)
+        self.fc3 = nn.Linear(200, 50)
+        self.fc4 = nn.Linear(50, 10)
+        self.fc5 = nn.Linear(10, 5)
 
     def forward(self, x):
-        x = F.max_pool2d(F.relu(self.cv1(x)), (3, 3))
-        x = F.max_pool2d(F.relu(self.cv2(x)), (3, 3))
+        x = F.max_pool2d(F.relu(self.cv1(x)), (1, 6))
+        x = F.max_pool2d(F.relu(self.cv2(x)), (1, 6))
         x = x.view(-1, reduce(lambda z, y: z * y, x.size()[1:], 1))
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        return F.softmax(self.fc3(x), dim=1)
+        x = F.relu(self.fc3(x))
+        x = F.relu(self.fc4(x))
+        return F.softmax(self.fc5(x), dim=1)
 
 
 def main(data_path=None, sentences=None, relearn=False, embeddings_path="embeddins.bin", block_length=20):
@@ -62,20 +66,23 @@ def main(data_path=None, sentences=None, relearn=False, embeddings_path="embeddi
     del sentences
     ent = Entwork()
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(ent.parameters(), lr=0.001, momentum=0.9)
+    optimizer = optim.Adam(ent.parameters(), lr=0.001)
     print("Training...")
-    for epoch in range(10):
-        running_loss = 0.0
+    for epoch in range(100):
+        success = 0
+        total = 0
         for i, data in enumerate(train_data_loader):
             labels, inputs = data
-            inputs, labels = Variable(inputs), Variable(labels.long())
+            inputs_var, labels_var = Variable(inputs), Variable(labels.long())
             optimizer.zero_grad()
-            outs = ent(inputs)
-            loss = criterion(outs, labels)
+            outs = ent(inputs_var)
+            _, preds = torch.max(outs.data, 1)
+            success += preds.eq(labels).sum()
+            total += len(labels)
+            loss = criterion(outs, labels_var)
             loss.backward()
             optimizer.step()
-            running_loss += loss.data[0]
-        print("epoch {} loss: {}".format(epoch, running_loss))
+        print("epoch {} success rate: {}".format(epoch, (success / total)))
     print("Evaluating model accuracy...")
     c = 0
     for i, data in enumerate(test_data_loader):
@@ -143,4 +150,7 @@ if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Please specify file path")
     else:
+        start_time = time.time()
         main(sys.argv[1])
+        passed_time = time.time() - start_time
+        print('procedure took {} hours'.format(str(passed_time / 60) / 60 ))
