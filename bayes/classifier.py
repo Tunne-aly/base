934d34b65dfa5d1e22c9ddf6a7b65425ed215a29
Ville-Veikko Saari <ville-veikko.saari@cs.helsinki.fi>
2018-06-15 21:33:11 +0300

    bayes classifier

--- bayes/classifier.py
+++ bayes/classifier.py
@@ -9,6 +9,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfTransformer
 import sklearn
 import nltk
+import matplotlib.pyplot as plt
+import itertools
 
 import csv
 import argparse
@@ -19,10 +21,11 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("file", help="Input file or folder")
     parser.add_argument("-f", "--folder", action="store_true", help="Data is from entire folder")
-    parser.add_argument("-b", "--balance", help="Balance data for bad/good reviews")
-    parser.add_argument("-d", "--data", help="Column of csv for the data")
-    parser.add_argument("-k", "--key", help="Column of csv for the key")
+    parser.add_argument("-b", "--balance", action="store_true", help="Balance data for bad/good reviews")
+    parser.add_argument("-d", "--data", help="Column of csv for the data", default=1)
+    parser.add_argument("-k", "--key", help="Column of csv for the key", default=0)
     parser.add_argument("-o", "--output", help="Output filename")
+    parser.add_argument("-m", "--mode", dest="mode", help="2, 3 or 5 classes.", default=2)
     args = parser.parse_args()
     file_path = args.file
     reviews = pd.Series([])
@@ -33,26 +36,54 @@ def main():
         reviews = read_csv(file_path)
 
     print(reviews.shape)
-    reviews = reviews[reviews['key'] != 3]
-    reviews['key'] = np.where(reviews['key']<3,0,1)
-    positive = reviews[reviews['key'] == 1]
-    negative = reviews[reviews['key'] == 0]
 
-    maximum = min(len(positive), len(negative))
-    print("Max: " + str(maximum))
+    if(int(args.mode) == 2):
+        reviews = reviews[reviews['key'] != 3]
+        reviews['key'] = np.where(reviews['key']<3,0,1)
+    elif(int(args.mode) == 3):
+        reviews['key'] = np.where(reviews['key']<3, -1, reviews['key'])
+        reviews['key'] = np.where(reviews['key']==3, 0, reviews['key'])
+        reviews['key'] = np.where(reviews['key']>3, 1, reviews['key'])
+
+
+    keys = reviews.key.unique()
+    # positive = reviews[reviews['key'] == 1]
+    # neutral = reviews[reviews['key'] == 0]
+    # negative = reviews[reviews['key'] == -1]
+
+    if args.balance:
+        maximum = 1000000
+        for key in keys:
+            print(key)
+            maximum = min(maximum, len(reviews[reviews['key'] == key]))
+
+        # print("positive", positive.shape)
+        # print("neutral", neutral.shape)
+        # print("negative", negative.shape)
+
+        # maximum = min(len(positive), len(negative), len(neutral))
+        print("Balanced to: " + str(maximum))
+
+        #np.random.shuffle(negative)
+        newReviews = pd.Series([])
+        for key in keys:
+            newReviews = pd.concat([newReviews, reviews[reviews['key'] == key].sample(n=maximum)])
+
+        # positive = positive.sample(n=maximum)
+        # negative = negative.sample(n=maximum)
+        # neutral = neutral.sample(n=maximum)
+        print(reviews)
+        print(newReviews)
+        # reviews = pd.concat([positive, negative, neutral])
+        reviews = newReviews
 
     
-    
-    #np.random.shuffle(negative)
-    positive = positive.sample(n=maximum)
-    negative = negative.sample(n=maximum)
-
-    reviews = pd.concat([positive, negative])
 
     counts = reviews['key'].value_counts()
     print(counts)
-    print(counts[0]/len(reviews))
-    print(counts[1]/len(reviews))
+    # print(counts[0]/len(reviews))
+    # print(counts[1]/len(reviews))
+    # print(counts[-1]/len(reviews))
 
 
     stop_words_fi=["aiemmin","aika","aikaa","aikaan","aikaisemmin","aikaisin","aikajen","aikana","aikoina","aikoo","aikovat","aina","ainakaan","ainakin","ainoa","ainoat","aiomme","aion","aiotte","aist","aivan","ajan","alas","alemmas","alkuisin","alkuun","alla","alle","aloitamme","aloitan","aloitat","aloitatte","aloitattivat","aloitettava","aloitettevaksi","aloitettu","aloitimme","aloitin","aloitit","aloititte","aloittaa","aloittamatta","aloitti","aloittivat","alta","aluksi","alussa","alusta","annettavaksi","annetteva","annettu","ansiosta","antaa","antamatta","antoi","aoua","apu","asia","asiaa","asian","asiasta","asiat","asioiden","asioihin","asioita","asti","avuksi","avulla","avun","avutta","edelle","edelleen","edellä","edeltä","edemmäs","edes","edessä","edestä","ehkä","ei","eikä","eilen","eivät","eli","ellei","elleivät","ellemme","ellen","ellet","ellette","emme","en","enemmän","eniten","ennen","ensi","ensimmäinen","ensimmäiseksi","ensimmäisen","ensimmäisenä","ensimmäiset","ensimmäisiksi","ensimmäisinä","ensimmäisiä","ensimmäistä","ensin","entinen","entisen","entisiä","entisten","entistä","enää","eri","erittäin","erityisesti","eräiden","eräs","eräät","esi","esiin","esillä","esimerkiksi","et","eteen","etenkin","etessa","ette","ettei","että","haikki","halua","haluaa","haluamatta","haluamme","haluan","haluat","haluatte","haluavat","halunnut","halusi","halusimme","halusin","halusit","halusitte","halusivat","halutessa","haluton","he","hei","heidän","heidät","heihin","heille","heillä","heiltä","heissä","heistä","heitä","helposti","heti","hetkellä","hieman","hitaasti","hoikein","huolimatta","huomenna","hyvien","hyviin","hyviksi","hyville","hyviltä","hyvin","hyvinä","hyvissä","hyvistä","hyviä","hyvä","hyvät","hyvää","hän","häneen","hänelle","hänellä","häneltä","hänen","hänessä","hänestä","hänet","häntä","ihan","ilman","ilmeisesti","itse","itsensä","itseään","ja","jo","johon","joiden","joihin","joiksi","joilla","joille","joilta","joina","joissa","joista","joita","joka","jokainen","jokin","joko","joksi","joku","jolla","jolle","jolloin","jolta","jompikumpi","jona","jonka","jonkin","jonne","joo","jopa","jos","joskus","jossa","josta","jota","jotain","joten","jotenkin","jotenkuten","jotka","jotta","jouduimme","jouduin","jouduit","jouduitte","joudumme","joudun","joudutte","joukkoon","joukossa","joukosta","joutua","joutui","joutuivat","joutumaan","joutuu","joutuvat","juuri","jälkeen","jälleen","jää","kahdeksan","kahdeksannen","kahdella","kahdelle","kahdelta","kahden","kahdessa","kahdesta","kahta","kahteen","kai","kaiken","kaikille","kaikilta","kaikkea","kaikki","kaikkia","kaikkiaan","kaikkialla","kaikkialle","kaikkialta","kaikkien","kaikkin","kaksi","kannalta","kannattaa","kanssa","kanssaan","kanssamme","kanssani","kanssanne","kanssasi","kauan","kauemmas","kaukana","kautta","kehen","keiden","keihin","keiksi","keille","keillä","keiltä","keinä","keissä","keistä","keitten","keittä","keitä","keneen","keneksi","kenelle","kenellä","keneltä","kenen","kenenä","kenessä","kenestä","kenet","kenettä","kennessästä","kenties","kerran","kerta","kertaa","keskellä","kesken","keskimäärin","ketkä","ketä","kiitos","kohti","koko","kokonaan","kolmas","kolme","kolmen","kolmesti","koska","koskaan","kovin","kuin","kuinka","kuinkan","kuitenkaan","kuitenkin","kuka","kukaan","kukin","kukka","kumpainen","kumpainenkaan","kumpi","kumpikaan","kumpikin","kun","kuten","kuuden","kuusi","kuutta","kylliksi","kyllä","kymmenen","kyse","liian","liki","lisäksi","lisää","lla","luo","luona","lähekkäin","lähelle","lähellä","läheltä","lähemmäs","lähes","lähinnä","lähtien","läpi","mahdollisimman","mahdollista","me","meidän","meidät","meihin","meille","meillä","meiltä","meissä","meistä","meitä","melkein","melko","menee","meneet","menemme","menen","menet","menette","menevät","meni","menimme","menin","menit","menivät","mennessä","mennyt","menossa","mihin","mikin","miksi","mikä","mikäli","mikään","mille","milloin","milloinkan","millä","miltä","minkä","minne","minua","minulla","minulle","minulta","minun","minussa","minusta","minut","minuun","minä","missä","mistä","miten","mitkä","mitä","mitään","moi","molemmat","mones","monesti","monet","moni","moniaalla","moniaalle","moniaalta","monta","muassa","muiden","muita","muka","mukaan","mukaansa","mukana","mutta","muu","muualla","muualle","muualta","muuanne","muulloin","muun","muut","muuta","muutama","muutaman","muuten","myöhemmin","myös","myöskin","myöskään","myötä","ne","neljä","neljän","neljää","niiden","niihin","niiksi","niille","niillä","niiltä","niin","niinä","niissä","niistä","niitä","noiden","noihin","noiksi","noilla","noille","noilta","noin","noina","noissa","noista","noita","nopeammin","nopeasti","nopeiten","nro","nuo","nyt","näiden","näihin","näiksi","näille","näillä","näiltä","näin","näinä","näissä","näissähin","näissälle","näissältä","näissästä","näistä","näitä","nämä","ohi","oikea","oikealla","oikein","ole","olemme","olen","olet","olette","oleva","olevan","olevat","oli","olimme","olin","olisi","olisimme","olisin","olisit","olisitte","olisivat","olit","olitte","olivat","olla","olleet","olli","ollut","oma","omaa","omaan","omaksi","omalle","omalta","oman","omassa","omat","omia","omien","omiin","omiksi","omille","omilta","omissa","omista","on","onkin","onko","ovat","paikoittain","paitsi","pakosti","paljon","paremmin","parempi","parhaillaan","parhaiten","perusteella","peräti","pian","pieneen","pieneksi","pienelle","pienellä","pieneltä","pienempi","pienestä","pieni","pienin","poikki","puolesta","puolestaan","päälle","runsaasti","saakka","sadam","sama","samaa","samaan","samalla","samallalta","samallassa","samallasta","saman","samat","samoin","sata","sataa","satojen","se","seitsemän","sekä","sen","seuraavat","siellä","sieltä","siihen","siinä","siis","siitä","sijaan","siksi","sille","silloin","sillä","silti","siltä","sinne","sinua","sinulla","sinulle","sinulta","sinun","sinussa","sinusta","sinut","sinuun","sinä","sisäkkäin","sisällä","siten","sitten","sitä","ssa","sta","suoraan","suuntaan","suuren","suuret","suuri","suuria","suurin","suurten","taa","taas","taemmas","tahansa","tai","takaa","takaisin","takana","takia","tallä","tapauksessa","tarpeeksi","tavalla","tavoitteena","te","teidän","teidät","teihin","teille","teillä","teiltä","teissä","teistä","teitä","tietysti","todella","toinen","toisaalla","toisaalle","toisaalta","toiseen","toiseksi","toisella","toiselle","toiselta","toisemme","toisen","toisensa","toisessa","toisesta","toista","toistaiseksi","toki","tosin","tuhannen","tuhat","tule","tulee","tulemme","tulen","tulet","tulette","tulevat","tulimme","tulin","tulisi","tulisimme","tulisin","tulisit","tulisitte","tulisivat","tulit","tulitte","tulivat","tulla","tulleet","tullut","tuntuu","tuo","tuohon","tuoksi","tuolla","tuolle","tuolloin","tuolta","tuon","tuona","tuonne","tuossa","tuosta","tuota","tuotä","tuskin","tykö","tähän","täksi","tälle","tällä","tällöin","tältä","tämä","tämän","tänne","tänä","tänään","tässä","tästä","täten","tätä","täysin","täytyvät","täytyy","täällä","täältä","ulkopuolella","usea","useasti","useimmiten","usein","useita","uudeksi","uudelleen","uuden","uudet","uusi","uusia","uusien","uusinta","uuteen","uutta","vaan","vahemmän","vai","vaiheessa","vaikea","vaikean","vaikeat","vaikeilla","vaikeille","vaikeilta","vaikeissa","vaikeista","vaikka","vain","varmasti","varsin","varsinkin","varten","vasen","vasenmalla","vasta","vastaan","vastakkain","vastan","verran","vielä","vierekkäin","vieressä","vieri","viiden","viime","viimeinen","viimeisen","viimeksi","viisi","voi","voidaan","voimme","voin","voisi","voit","voitte","voivat","vuoden","vuoksi","vuosi","vuosien","vuosina","vuotta","vähemmän","vähintään","vähiten","vähän","välillä","yhdeksän","yhden","yhdessä","yhteen","yhteensä","yhteydessä","yhteyteen","yhtä","yhtäälle","yhtäällä","yhtäältä","yhtään","yhä","yksi","yksin","yksittäin","yleensä","ylemmäs","yli","ylös","ympäri","älköön","älä"]
@@ -69,23 +100,23 @@ def main():
 
     X_train = X_train.todense()
     X_test = X_test.todense()
-    print("Gaussian")
-    gauss = GaussianNB()
+    # print("Gaussian")
+    # gauss = GaussianNB()
 
-    gauss.fit(
-        X_train,
-        y_train
-    )
+    # gauss.fit(
+    #     X_train,
+    #     y_train
+    # )
 
 
    
-    pred = gauss.predict(X_test)
-    print(sklearn.metrics.accuracy_score(y_test, pred))
+    # pred = gauss.predict(X_test)
+    # print(sklearn.metrics.accuracy_score(y_test, pred))
 
 
     from sklearn.metrics import confusion_matrix
-    cm = confusion_matrix(y_test, pred)
-    print(cm)
+    # cm = confusion_matrix(y_test, pred)
+    # print(cm)
 
     print("Multinomial")
     multi = MultinomialNB()
@@ -93,29 +124,72 @@ def main():
         X_train,
         y_train
     )
-
+    print(multi.classes_)
     pred = multi.predict(X_test)
+    print(multi.predict_proba(X_test))
     print(sklearn.metrics.accuracy_score(y_test, pred))
     cm = confusion_matrix(y_test, pred)
     print(cm)
 
-    print("Bernoulli")
-    bern = BernoulliNB()
-    bern.fit(
-        X_train,
-        y_train
-    )
+    plt.figure()
+    # plot_confusion_matrix(cm, ['Negative', 'Neutral', 'Positive'])
+    plot_confusion_matrix(cm, keys)
 
-    pred = bern.predict(X_test)
-    print(sklearn.metrics.accuracy_score(y_test, pred))
-    cm = confusion_matrix(y_test, pred)
-    print(cm)
+    plt.show()
+
+    # print("Bernoulli")
+    # bern = BernoulliNB()
+    # bern.fit(
+    #     X_train,
+    #     y_train
+    # )
+
+    # pred = bern.predict(X_test)
+    # print(sklearn.metrics.accuracy_score(y_test, pred))
+    # cm = confusion_matrix(y_test, pred)
+    # print(cm)
+
+
 
 def read_csv(file):
     print("Loading file: " + file)
     return pd.read_csv(file, header=None, usecols=[0,1], names=["key", "data"])
 def output_file(data, file):
     return 0
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
 
 if __name__ == "__main__":
     main()
